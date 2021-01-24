aa3aa94df53c6e207f1f602575e6982a3ab6dfcf
mpasko <mpasko78@gmail.com>
2021-01-24 12:39:20 -0600

    Added Redux Tools for store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -10,6 +10,8 @@ if (persistedStore) {
   }
 }
 
-const store = createStore(rootReducer, preloadedState);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, preloadedState, composeEnhancers);
 
 export default store;
